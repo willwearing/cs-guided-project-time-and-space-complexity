1e39db70e97443a205560894b0c7696b6b45a8f4
willwearing <willwearing@gmail.com>
2020-12-02 12:48:50 -0700

    lesson finished

--- src/demonstration_01.py
+++ src/demonstration_01.py
@@ -3,7 +3,7 @@ Given an array of integers `nums` and an integer `target`, return the indices
 of the two numbers that add up to the `target`.
 
 Examples:
-
+- two_sum(nums = [2,5,9,13], target = 18) -> [1, 3]
 - two_sum(nums = [2,5,9,13], target = 7) -> [0,1] (nums[0] + nums[1] == 7)
 - two_sum(nums = [4,3,5], target = 8) -> [1,2] (nums[1] + nums[2] == 8)
 
@@ -13,6 +13,41 @@ Notes:
 - You may not use the same element twice.
 - You can return the answer in any order.
 """
+#SECOND TRY HERE:
+#how can we write this with only a single for loop that 
+#still gives us the same answer?
 def two_sum(nums, target):
-    # Your code here
 
+def two_sum_better(nums, target):
+    d = {}
+    for index, num in enumerate(nums):  # O(n)
+        d[num] = index
+    for index, num in enumerate(nums):  # O(n)
+        diff = target - num
+        if diff in d:
+            if diff == num: 
+                continue
+            return [index, d[diff]]
+        return [-1, -1]
+print(two_sum_better([4, 5, 3, 6], 8))
+
+
+#FIRST TRY HERE:
+# def two_sum(nums, target):
+#     #using nested for loops, we can iterate over the nums iterate over all 
+#     #the other nums
+#         #if the two numbers sum up to our target
+#         #return their indices
+
+#     #O(n * n) == O(n^2) 
+#     for i in range(len(nums)): #O(n)
+#         for j in range(len(nums)): #O(n)
+#             #add a check to make sure the indices are different
+#             if i == j: #O(1)
+#                 continue
+#             #O(1) + O(1) == O(2) ~ O(1)
+#             if nums[i] + nums[j] == target:
+#                 return [i, j]
+
+#     #we'll only get outside this for loop if we don't get an answer
+#     return[-1, -1]
--- src/demonstration_02.py
+++ src/demonstration_02.py
@@ -1,14 +1,44 @@
 """
 Demonstration #2
-
-Given a non-empty array of integers `nums`, every element appears twice except except for one. Write a function that finds the element that only appears once.
-
+​
+Given a non-empty array of integers `nums`, every element appears twice 
+except for one. Write a function that finds the element that only appears once.
+​
 Examples:
-
+​
 - single_number([3,3,2]) -> 2
+​
 - single_number([5,2,3,2,3]) -> 5
 - single_number([10]) -> 10
 """
 def single_number(nums):
     # Your code here
-
+    s = set()
+    # how could we save data to write a implementation that is more efficient? 
+    # for any given number, we'd like to know if we've seen this number already 
+    # can we somehow save each number we've seen in such a way that when we see 
+    # the next copy of it, we can access this quickly 
+​
+    # loop through nums 
+    for num in nums: # O(n)
+        # check if the num is in our set 
+        # if it is
+        if num in s: # O(1)
+            # remove it from our set 
+            s.remove(num) # O(1)
+        # if it isn't
+        else:
+            # add it to the set 
+            s.add(num) # O(1)
+        
+    # the only thing left in the set should be our odd-number-out 
+    # the only way to grab the odd-element-out from our set is to 
+    # turn it into a list and access it via its index 
+    return s.pop() # O(1) since our set should always only have 1 element 
+​
+    # O(n^2)
+    # we could use nested for loops to check 
+    # if every element has another copy of itself 
+    # in the list 
+        # the odd-number-out will not find a copy of itself 
+        # so that would be our answer 
